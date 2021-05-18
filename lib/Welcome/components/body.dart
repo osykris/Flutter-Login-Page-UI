185e94ff5c8a1c01eb75714fd08304a62b502fa8
osykri <osy.krisdayati27@gmail.com>
2021-05-18 15:15:25 +0700

    Mengedit layout dengan menambhakan welcome screen

---
+++ lib/Welcome/components/body.dart
@@ -0,0 +1,52 @@
+import 'package:day12_login/Login/login_screen.dart';
+import 'package:day12_login/Welcome/components/background.dart';
+import 'package:day12_login/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:day12_login/components/rounded_button.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "WELCOME TO CatatanKu",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "SIGN UP",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
